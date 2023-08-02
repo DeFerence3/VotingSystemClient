0cc51ee33e7326560d634ba0ac1d468da87cb25d
DeFerence3 <socialsabhishek@gmail.com>
2023-08-02 09:45:36 +0530

    ui improvements

--- app/src/main/java/com/proj/votingclient/fragments/HomeFragment.java
+++ app/src/main/java/com/proj/votingclient/fragments/HomeFragment.java
@@ -2,9 +2,7 @@ package com.proj.votingclient.fragments;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.graphics.Typeface;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -41,8 +39,11 @@ import java.util.Map;
 public class HomeFragment extends Fragment {
     static Integer f506id;
     String date, name, voterid;
+    Integer startTime, endTime;
+    Long startT, endT;
     LinearLayout electionview;
     LinearDotsLoader progressloader;
+    TextView tv_noelection;
     DatabaseOperations dbops = new DatabaseOperations();
     OnBackPressedCallback callback = new C21171(true);
 
@@ -58,6 +59,7 @@ public class HomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         this.electionview = view.findViewById(R.id.electionlinear);
         this.progressloader = view.findViewById(R.id.loadingprogress);
+        tv_noelection = view.findViewById(R.id.tv_noElection);
         read();
     }
 
@@ -65,79 +67,89 @@ public class HomeFragment extends Fragment {
         this.dbops.getAllElections().addOnSuccessListener(new OnSuccessListener() {
             @Override
             public void onSuccess(Object obj) {
-                HomeFragment.this.m827lambda$read$5$comtestonlinevotingsystemHomeFragment((QuerySnapshot) obj);
+                HomeFragment.this.electionDataPopulator((QuerySnapshot) obj);
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(Exception exc) {
-                Toast.makeText(getContext(), "Failed to Read Documents", Toast.LENGTH_SHORT).show();
+                tv_noelection.setText(R.string.failed_to_read_documents);
+                tv_noelection.setVisibility(View.VISIBLE);
+                progressloader.setVisibility(View.GONE);
+                electionview.setVisibility(View.GONE);
             }
         });
     }
 
-    public void m827lambda$read$5$comtestonlinevotingsystemHomeFragment(QuerySnapshot queryDocumentSnapshots) {
-        Iterator<QueryDocumentSnapshot> it = queryDocumentSnapshots.iterator();
-        while (it.hasNext()) {
-            DocumentSnapshot documentSnapshot = it.next();
-            this.name = documentSnapshot.getString("Name");
-            this.date = documentSnapshot.getString(HttpHeaders.DATE);
-            f506id = Integer.valueOf(documentSnapshot.getId());
-            Long startT = (Long) documentSnapshot.get("startTime");
-            Long endT = (Long) documentSnapshot.get("endTime");
-            Integer startTime = Integer.valueOf(Math.toIntExact(startT.longValue()));
-            Integer endTime = Integer.valueOf(Math.toIntExact(endT.longValue()));
-            StringBuilder timeframe = new StringBuilder();
-            timeframe.append(startTime);
-            timeframe.append(":00:00 to ");
-            timeframe.append(endTime);
-            timeframe.append(":00:00");
-            final LinearLayout linearLayout = new LinearLayout(getContext());
-            GradientDrawable shape = new GradientDrawable();
-            shape.setShape(GradientDrawable.RECTANGLE);
-            float[] cornerRadii = {40.0f, 40.0f, 40.0f, 40.0f, 40.0f, 40.0f, 40.0f, 40.0f};
-            shape.setCornerRadii(cornerRadii);
-            int colorint = Color.parseColor("#AA121212");
-            shape.setColors(new int[]{colorint, colorint});
-            linearLayout.setPadding(20, 10, 20, 5);
-            linearLayout.setOrientation(LinearLayout.VERTICAL);
-            linearLayout.setClickable(true);
-            linearLayout.setId(f506id.intValue());
-            LinearLayout.LayoutParams layoutParams1 = new LinearLayout.LayoutParams(-1, -2);
-            Iterator<QueryDocumentSnapshot> it2 = it;
-            layoutParams1.setMargins(0, 20, 0, 40);
-            linearLayout.setLayoutParams(layoutParams1);
-            linearLayout.setBackground(shape);
-            int textcolor = ContextCompat.getColor(requireContext(), R.color.lightgreytext);
-            TextView nameview = new TextView(getContext());
-            nameview.setText(this.name);
-            nameview.setTextSize(25.0f);
-            nameview.setTextColor(textcolor);
-            nameview.setPadding(5, 10, 0, 10);
-            nameview.setLayoutParams(new LinearLayout.LayoutParams(-1, -2));
-            linearLayout.addView(nameview);
-            TextView dateview = new TextView(getContext());
-            dateview.setText(this.date);
-            dateview.setTextSize(18.0f);
-            dateview.setTextColor(textcolor);
-            dateview.setPadding(5, 10, 0, 10);
-            dateview.setLayoutParams(new LinearLayout.LayoutParams(-1, -2));
-            linearLayout.addView(dateview);
-            TextView timeview = new TextView(getContext());
-            timeview.setText(timeframe);
-            timeview.setTextSize(18.0f);
-            timeview.setTextColor(textcolor);
-            timeview.setPadding(5, 10, 0, 15);
-            timeview.setLayoutParams(new LinearLayout.LayoutParams(-1, -2));
-            linearLayout.addView(timeview);
-            this.progressloader.setVisibility(View.GONE);
-            this.electionview.addView(linearLayout);
-            linearLayout.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public final void onClick(View view) {
-                    HomeFragment.this.m826lambda$read$4$comtestonlinevotingsystemHomeFragment(linearLayout, view);
-                }
-            });
-            it = it2;
+    public void electionDataPopulator(QuerySnapshot queryDocumentSnapshots) {
+        if (queryDocumentSnapshots.isEmpty()) {
+            tv_noelection.setVisibility(View.VISIBLE);
+            progressloader.setVisibility(View.GONE);
+            electionview.setVisibility(View.GONE);
+        } else {
+            Iterator<QueryDocumentSnapshot> it = queryDocumentSnapshots.iterator();
+            while (it.hasNext()) {
+                DocumentSnapshot documentSnapshot = it.next();
+                name = documentSnapshot.getString("Name");
+                date = documentSnapshot.getString(HttpHeaders.DATE);
+                f506id = Integer.valueOf(documentSnapshot.getId());
+                startT = (Long) documentSnapshot.get("startTime");
+                endT = (Long) documentSnapshot.get("endTime");
+                startTime = Math.toIntExact(startT);
+                endTime = Math.toIntExact(endT);
+
+                StringBuilder timeframe = new StringBuilder();
+                timeframe.append(startTime);
+                timeframe.append(":00:00 to ");
+                timeframe.append(endTime);
+                timeframe.append(":00:00");
+                final LinearLayout linearLayout = new LinearLayout(getContext());
+                GradientDrawable shape = new GradientDrawable();
+                shape.setShape(GradientDrawable.RECTANGLE);
+                float[] cornerRadii = {40.0f, 40.0f, 40.0f, 40.0f, 40.0f, 40.0f, 40.0f, 40.0f};
+                shape.setCornerRadii(cornerRadii);
+                int colorint = Color.parseColor("#AA121212");
+                shape.setColors(new int[]{colorint, colorint});
+                linearLayout.setPadding(20, 10, 20, 5);
+                linearLayout.setOrientation(LinearLayout.VERTICAL);
+                linearLayout.setClickable(true);
+                linearLayout.setId(f506id.intValue());
+                LinearLayout.LayoutParams layoutParams1 = new LinearLayout.LayoutParams(-1, -2);
+                Iterator<QueryDocumentSnapshot> it2 = it;
+                layoutParams1.setMargins(0, 20, 0, 40);
+                linearLayout.setLayoutParams(layoutParams1);
+                linearLayout.setBackground(shape);
+                int textcolor = ContextCompat.getColor(requireContext(), R.color.lightgreytext);
+                TextView nameview = new TextView(getContext());
+                nameview.setText(this.name);
+                nameview.setTextSize(25.0f);
+                nameview.setTextColor(textcolor);
+                nameview.setPadding(5, 10, 0, 10);
+                nameview.setLayoutParams(new LinearLayout.LayoutParams(-1, -2));
+                linearLayout.addView(nameview);
+                TextView dateview = new TextView(getContext());
+                dateview.setText(this.date);
+                dateview.setTextSize(18.0f);
+                dateview.setTextColor(textcolor);
+                dateview.setPadding(5, 10, 0, 10);
+                dateview.setLayoutParams(new LinearLayout.LayoutParams(-1, -2));
+                linearLayout.addView(dateview);
+                TextView timeview = new TextView(getContext());
+                timeview.setText(timeframe);
+                timeview.setTextSize(18.0f);
+                timeview.setTextColor(textcolor);
+                timeview.setPadding(5, 10, 0, 15);
+                timeview.setLayoutParams(new LinearLayout.LayoutParams(-1, -2));
+                linearLayout.addView(timeview);
+                this.progressloader.setVisibility(View.GONE);
+                this.electionview.addView(linearLayout);
+                linearLayout.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public final void onClick(View view) {
+                        HomeFragment.this.m826lambda$read$4$comtestonlinevotingsystemHomeFragment(linearLayout, view);
+                    }
+                });
+                it = it2;
+            }
         }
     }
 
